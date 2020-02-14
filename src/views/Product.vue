c839ac9865aba197c13904d25aecd5e94807397a
headlulu888 <headlulu888@gmail.com>
2020-02-14 13:20:06 +0300

    add product page && route

---
+++ src/views/Product.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about">
+    <h1>This is an product page</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Product',
+    mounted() {
+      console.log('Product is mounted');
+    }
+  }
+
+</script>
